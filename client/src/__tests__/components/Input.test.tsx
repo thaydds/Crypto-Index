64fe11f641503993d182edf74c2e033cded3796f
thaydds <thayronedds@gmail.com>
2021-02-14 01:07:15 -0300

    add test for Input component

---
+++ client/src/__tests__/components/Input.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { Input } from '../../components';
+
+describe('<Input/>', () => {
+  it('should render the Input', () => {
+    const { getByPlaceholderText } = render(
+      <Input type="text" placeholder="test" />,
+    );
+    const input = getByPlaceholderText('test');
+
+    expect(input).toBeInTheDocument();
+  });
+  it('should render the CurrencyInput', () => {
+    const { getByDisplayValue } = render(
+      <Input type="text" placeholder="test" isCurrency value="0" />,
+    );
+    const input = getByDisplayValue('$0');
+
+    expect(input).toBeInTheDocument();
+  });
+});
